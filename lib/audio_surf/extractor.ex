667bd42f7345356411337cd69dbc34ff72da4895
pmargreff <pmargreff@gmail.com>
2018-03-28 06:15:01 -0300

    Alter behavior on extractors.
    	* Passing frames if need to filter channel.
    	* Add function actions inline.

--- lib/audio_surf/extractor.ex
+++ lib/audio_surf/extractor.ex
@@ -4,24 +4,21 @@ defmodule AudioSurf.Extractor do
   @moduledoc """
   Documentation for AudioSurf.Extractor
   """
+  def channel(frames, :left), do: extract_channel(frames, 0)
+
+  def channel(frames, :right), do: extract_channel(frames, 1)
+
+  defp extract_channel(frames, channel_number) do
+    Enum.map(frames, fn x -> Enum.at(x, channel_number) end)
+  end
+
+  def frame(%Audio{} = audio, offset \\ 0) do
+    frames(audio) |> Enum.at(offset)
+  end
 
   def frames(%Audio{data: data, bits_per_sample: bits_per_sample}) do
     for <<left::little-integer-signed-size(bits_per_sample),
           rigth::little-integer-signed-size(bits_per_sample) <- data>>,
         do: [left, rigth]
-  end
-
-  def frames(%Audio{} = audio, :left) do
-    frames(audio)
-    |> Enum.map(fn x -> Enum.at(x, 0) end)
-  end
-
-  def frames(%Audio{} = audio, :right) do
-    frames(audio)
-    |> Enum.map(fn x -> Enum.at(x, 1) end)
-  end
-
-  def frame(%Audio{} = audio, offset \\ 0) do
-    frames(audio) |> Enum.at(offset)
   end
 end
