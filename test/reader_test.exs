f5a138c4bc555279fc563a2a5421a78fe81950ac
pmargreff <pmargreff@gmail.com>
2018-03-31 23:40:10 -0300

    Adding basic specs to open file
    	* Adding specs to fail and succesfull open file.
    	* Adding dual channel stub file.

---
+++ test/reader_test.exs
@@ -0,0 +1,38 @@
+defmodule AudioSurfReaderTest do
+  use ExUnit.Case
+
+  doctest AudioSurf.Reader
+
+  test "open/1 file doesn't exists" do
+    filepath = "doesnt_exist.txt"
+    error_message = "There was an error on #{filepath}: enoent"
+
+    assert AudioSurf.Reader.open(filepath) == {:error, error_message}
+  end
+
+  test "open/1 open file" do
+    filepath = "#{File.cwd!()}/test/dual_channel_stub.wav"
+
+    {:ok, audio} = AudioSurf.Reader.open(filepath)
+
+    assert audio.audio_format == 1
+    assert audio.bits_per_sample == 16
+    assert audio.block_align == 4
+    assert audio.byte_rate == 176_400
+    assert audio.chunk_id == "RIFF"
+    assert audio.chunk_size == 2_677_458
+    assert audio.data =~ data_chunk()
+    assert audio.format == "WAVE"
+    assert audio.num_channels == 2
+    assert audio.sample_rate == 44_100
+    assert audio.sub_chunk_1_id == "fmt "
+    assert audio.sub_chunk_1_size == 16
+    assert audio.sub_chunk_2_id == "LIST"
+    assert audio.sub_chunk_2_size == 166
+  end
+
+  defp data_chunk do
+    <<73, 78, 70, 79, 73, 65, 82, 84, 17, 0, 0, 0, 72, 101, 110, 100, 114, 105, 107, 32, 66, 114,
+      111, 101, 107, 109, 97, 110, 0, 0, 73, 71, 78, 82, 26, 0, 0, 0, 72, 117, 98>>
+  end
+end
