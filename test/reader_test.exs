d16d5f82c6d5ffaa662314e445b46a864a1a3899
pmargreff <pmargreff@gmail.com>
2018-04-01 00:39:26 -0300

    adding some specifications specs

--- test/reader_test.exs
+++ test/reader_test.exs
@@ -31,6 +31,17 @@ defmodule AudioSurfReaderTest do
     assert audio.sub_chunk_2_size == 166
   end
 
+  # test the file on the specification, whoch allow to derivate
+  # some field with other fields values
+  # https://web.archive.org/web/20141213140451/https://ccrma.stanford.edu/courses/422/projects/WaveFormat/
+  test "read/1 assert the format specifications" do
+    filepath = "#{File.cwd!()}/test/dual_channel_stub.wav"
+
+    {:ok, audio} = AudioSurf.Reader.read(filepath)
+    assert audio.block_align == audio.num_channels * (audio.bits_per_sample / 8 |> round())
+    assert audio.byte_rate == audio.sample_rate * audio.block_align
+  end
+
   defp data_chunk do
     <<73, 78, 70, 79, 73, 65, 82, 84, 17, 0, 0, 0, 72, 101, 110, 100, 114, 105, 107, 32, 66, 114,
       111, 101, 107, 109, 97, 110, 0, 0, 73, 71, 78, 82, 26, 0, 0, 0, 72, 117, 98>>
